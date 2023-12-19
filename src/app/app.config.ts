f753436fe230cde8943d129d130a20b24834fef8
axherrm <axel-herrmann@gmx.net>
2023-12-19 01:16:33 +0100

    make image responsive

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {IMAGE_LOADER, ImageLoaderConfig} from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations()]
+  providers: [
+    provideAnimations(),
+    {
+      provide: IMAGE_LOADER,
+      useValue: (config: ImageLoaderConfig) => {
+        return `${location.origin}/${config.src}_${config.width}.png`;
+      }
+    }
+  ]
 };
