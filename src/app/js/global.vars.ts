fd3a29eb68a1768b3294a883c0df729508a7ca7f
axherrm <axel-herrmann@gmx.net>
2024-02-02 17:37:21 +0100

    move core to submodule

--- src/app/js/global.vars.ts
+++
@@ -1,5 +0,0 @@
-import packageJson from '../../../package.json';
-
-export const appVersion: string = packageJson.version;
-export const appName: string = "WebCVTemplate";
-export const githubURL: string = "https://github.com/axherrm/CV";
